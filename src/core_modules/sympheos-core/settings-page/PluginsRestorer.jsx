b02c68c91c5cd8bddf619fe80903a6c41e902084
Marlpzg <milopezg@uca.edu.sv>
2025-07-15 22:57:08 -0600

    chore: Display confirmation message before restoring plugin configs

--- src/core_modules/sympheos-core/settings-page/PluginsRestorer.jsx
+++ src/core_modules/sympheos-core/settings-page/PluginsRestorer.jsx
@@ -1,9 +1,17 @@
 // @flow
-import { Button } from '@dhis2/ui';
+import {
+    Button,
+    Modal,
+    ModalTitle,
+    ModalContent,
+    ModalActions,
+    ButtonStrip,
+    Box,
+} from '@dhis2/ui';
 import { useDataMutation, useDataQuery } from '@dhis2/app-runtime';
 import React, { useState } from 'react';
 import i18n from '@dhis2/d2-i18n';
-import { FiEdit } from 'react-icons/fi';
+import { FiAlertTriangle, FiEdit } from 'react-icons/fi';
 import { useSnackbar, SnackbarSeverity } from 'commons/Snackbar/SnackbarContext';
 
 import basePluginSettings from './pluginSettings';
@@ -49,12 +57,12 @@ const setPluginsAppUrl = (pluginSettings: any, appUrl: string) => {
 const PluginsRestorer = () => {
     const { showSnackbar } = useSnackbar();
     const [isLoading, setIsLoading] = useState(false);
+    const [hide, setHide] = useState(true);
 
     const [
         mutateEnrollmentOverviewLayout,
         {
             loading: loadingUpdate,
-            error: errorUpdate,
         },
     ] = useDataMutation(updateQuery, {
         // eslint-disable-next-line no-console
@@ -66,7 +74,6 @@ const PluginsRestorer = () => {
 
     const {
         loading: loadingPrograms,
-        error: errorPrograms,
         refetch: refetchPrograms,
     } = useDataQuery(programsQuery, {
         // eslint-disable-next-line no-console
@@ -126,20 +133,50 @@ const PluginsRestorer = () => {
                         severity: SnackbarSeverity.CRITICAL,
                     });
                 }
+                setHide(true);
                 setIsLoading(false);
             });
     };
 
-    return (<Button
-        destructive
-        secondary
-        onClick={handleRestorePlugins}
-        loading={isLoading}
-        disabled={loadingUpdate || loadingPrograms || baseUrlLoading}
-        icon={<FiEdit />}
-    >
-        {i18n.t('Overwrite plugins configuration for all Programs')}
-    </Button>);
+    return (<>
+        <Modal hide={hide} onClose={() => setHide(true)}>
+            <ModalTitle>{i18n.t('Confirmation')}</ModalTitle>
+            <ModalContent>
+                <Box>
+                    {i18n.t('Are you sure that you want to overwrite plugins configuration for all Programs? This action cannot be undone and will remove the current plugins configuration.')}
+                </Box>
+            </ModalContent>
+            <ModalActions>
+                <ButtonStrip end>
+                    <Button
+                        onClick={() => setHide(true)}
+                        secondary
+                        disabled={isLoading}
+                    >
+                        {i18n.t('Cancel')}
+                    </Button>
+                    <Button
+                        onClick={handleRestorePlugins}
+                        destructive
+                        loading={isLoading}
+                        icon={<FiAlertTriangle />}
+                    >
+                        {i18n.t('Continue')}
+                    </Button>
+                </ButtonStrip>
+            </ModalActions>
+        </Modal>
+        <Button
+            destructive
+            secondary
+            onClick={() => setHide(false)}
+            loading={isLoading}
+            disabled={loadingUpdate || loadingPrograms || baseUrlLoading}
+            icon={<FiEdit />}
+        >
+            {i18n.t('Overwrite plugins configuration for all Programs')}
+        </Button>
+    </>);
 };
 
 export default PluginsRestorer;
